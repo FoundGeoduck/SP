f412cc1df70858970d97c37b5a1e2203bcb385a3
FoundGeoduck <dan181299@mail.ru>
2023-03-20 02:19:52 -0400

    lab2

--- lab2/lab2_Server.py
+++ lab2/lab2_Server.py
@@ -1,14 +1,14 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print ('Write IP address of Client, IP: ')
+print ('Write IP address of Client: ')
 ipc = input()
 print ('Write port of Client ')
 port = (int(input()))
 sock.bind((ipc, port))
 print ('Server is running, please, press ctrl+c to stop')
 while True:
-	conn,addr = sock.accept()
+	conn, addr = sock.accept()
 	print ('connected: ', addr)
 	data = conn.recv(1024)
 	print (str(data))
