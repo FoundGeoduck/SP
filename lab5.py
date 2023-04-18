b2a56b71f947395c3ce8aeed933faa5376fe0985
FoundGeoduck <dan181299@mail.ru>
2023-04-18 01:45:52 -0400

    lab5

---
+++ lab5.py
@@ -0,0 +1,13 @@
+from ftplib import FTP
+ 
+ftp = FTP('ftp.gnu.org')
+
+ftp.login() 
+
+ 
+# Путь на нашем компьютере где сохранить файл.
+out = '/root/recieve/tree.json.gz'
+ 
+with open(out, 'wb') as f:
+    ftp.retrbinary('RETR ' + 'tree.json.gz', f.write)
+
